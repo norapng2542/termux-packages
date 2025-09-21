7189273e9589c8950396de29e220d60bfc59c3ef
norapng2542 <110341113+norapng2542@users.noreply.github.com>
2025-09-21 16:15:58 +0700

    Update build.sh

--- root-packages/i2c-tools/build.sh
+++ root-packages/i2c-tools/build.sh
@@ -7,3 +7,4 @@ TERMUX_PKG_SRCURL="https://mirrors.edge.kernel.org/pub/software/utils/i2c-tools/
 TERMUX_PKG_SHA256=8b15f0a880ab87280c40cfd7235cfff28134bf14d5646c07518b1ff6642a2473
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
+API_GAME_PKG_AUTO=true
